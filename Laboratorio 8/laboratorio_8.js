e14267999ba1df13215f42b805b5fd1d247700d9
Karen Baltazar <a01707750@tec.mx>
2022-02-27 22:19:00 -0600

    L8: E2. Archivo de texto

--- Laboratorio 8/laboratorio_8.js
+++ Laboratorio 8/laboratorio_8.js
@@ -1,4 +1,4 @@
-// Función 1. Promedio.
+// Función 1. Promedio. (node .\laboratorio_8.js)
 function ejer_1(array) {
     let summatory = 0;
     for (let element of arr) {
@@ -18,12 +18,12 @@ let resultado = ejer_1(arr);
 console.log('Arreglo: ' + '[' + arr.toString() + ']')
 console.log('Promedio: ' + resultado);
 
-// Función 2. 
-//En construcción. Función que reciba un string y escriba el string en un archivo de texto. Apóyate del módulo fs.
-let texto = 'fdjflksdjflsd';
+// Función 2. Archivo de texto (módulo fs).
+let texto = '"Mine has been a life of much shame. I can';
+texto += "'t" + 'even guess myself what it must be to live the life of a human being."';
+texto +="\n― Osamu Dazai, No Longer Human";
 const filesystem = require('fs');
 filesystem.writeFileSync('laboratorio.txt', texto);
-
 
 // Función 3. 
 // En construcción. Escoge algún problema que hayas implementado en otro lenguaje de programación, y dale una solución 
