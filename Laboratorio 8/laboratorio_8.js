1fba60841ecd2607da68a53f9c9f1572e1b9faae
Karen Baltazar <a01707750@tec.mx>
2022-02-27 23:01:58 -0600

    L8: Servidor y aplicación web

--- Laboratorio 8/laboratorio_8.js
+++ Laboratorio 8/laboratorio_8.js
@@ -20,15 +20,34 @@ console.log('Promedio: ' + resultado);
 
 // Función 2. Archivo de texto (módulo fs).
 let texto = '"Mine has been a life of much shame. I can';
-texto += "'t" + 'even guess myself what it must be to live the life of a human being."';
-texto +="\n― Osamu Dazai, No Longer Human";
+texto += "'t" + 'even guess myself what it must be to live';
+texto += 'the life of a human being."\n― Osamu Dazai, No Longer Human';
 const filesystem = require('fs');
 filesystem.writeFileSync('laboratorio.txt', texto);
 
 // Función 3. 
-// En construcción. Escoge algún problema que hayas implementado en otro lenguaje de programación, y dale una solución 
-// en js que se ejecute sobre node.
+// En construcción. Problema de elección.
 
-// Servidor. 
-// En construcción. Aplicación web que al enviar una petición al servidor, devuelva una de las páginas que creaste 
-// anteriormente en tus laboratorios
+// Función 4. Petición al servidor. http://localhost:3000/
+const http = require('http');
+const fs = require('fs').promises;
+
+const server = http.createServer( (request, response) => {
+    fs.readFile(__dirname + "/laboratorio.html")
+    .then(contents => {
+        response.setHeader('Content-Type', 'text/html');
+        response.writeHead(200);
+        response.end(contents);
+    })
+    .catch(err => {
+        response.writeHead(500);
+        response.end(err);
+        return;
+    });
+});
+
+server.listen(3000);
+
+// Referencias.
+// By. (2020). Cómo crear un servidor web en Node.js con el módulo HTTP. Recuperado de: 
+// https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module-es
