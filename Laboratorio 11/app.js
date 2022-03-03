3157c8942c5df1b1a92f14fea6c89a4dd8f7059a
Karen Baltazar <a01707750@tec.mx>
2022-03-02 22:23:58 -0600

    L11: Error 404

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -10,4 +10,11 @@ const rutas_fe = require('./routes/fe.routes.js');
 
 app.use('/fe', rutas_fe);
 
+app.get('*', (request, response, next) => {
+    console.log('Error de acceso a la ruta');
+    response.status(404);  
+    let respuesta = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Laboraotio 11</title></head><body><h1>FE | Sitio no encontrado.</h1><p>Lo siento, el elemento que buscas no existe. Para acceder a la magia de Fire Emblem, intenta tu suerte con la ruta: /fe</p></body>';
+    response.send(respuesta);
+});
+
 app.listen(3000);
