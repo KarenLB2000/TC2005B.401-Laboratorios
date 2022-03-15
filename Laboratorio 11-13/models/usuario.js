ffdc08981f6b4699d6695992e6d513be85a102ae
Karen Baltazar <a01707750@tec.mx>
2022-03-15 14:44:38 -0600

    L14: Modelo de Usuario

---
+++ Laboratorio 11-13/models/usuario.js
@@ -0,0 +1,23 @@
+// Laboratorio 14.
+const usuarios = [];
+
+module.exports = class User {
+
+    // Constructor de la clase y propiedades del modelo.
+    constructor(n_username, n_email, n_phone, n_password) {
+        this.username = n_username;
+        this.email = n_email;
+        this.phone = n_phone;
+        this.password = n_password;
+    }
+
+    //Método para guardar de manera persistente el nuevo objeto. 
+    save() {
+        usuarios.push(this);      
+    }
+
+    static login(username, password) {
+        return true;        
+    }
+
+}
