f3b163a4107c6a19adbb6d94a6e808165283bec4
Karen Baltazar <a01707750@tec.mx>
2022-03-15 09:44:26 -0600

    L14: Vistas de usuario

--- Laboratorio 11-13/controllers/usuario_controllers.js
+++ Laboratorio 11-13/controllers/usuario_controllers.js
@@ -2,13 +2,14 @@
 const path = require('path');
 
 exports.get_signup = (request, response, next) => {
-
+    response.render('signup.ejs');
 };
 
 exports.post_signup= (request, response, next) => {
-
+    response.render('singup.ejs');
+    response.redirect('/fe');
 };
 
 exports.login = (request, response, next) => {
-
+    response.render('login.ejs');
 };
