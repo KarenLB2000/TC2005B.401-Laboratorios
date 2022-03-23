07cae1d6c0ed6d38ddb0008710fe11c0f68bca31
Karen Baltazar <a01707750@tec.mx>
2022-03-22 20:19:05 -0600

    L17: Agregar usuarios a BD

--- Laboratorio 11-13/controllers/usuario_controllers.js
+++ Laboratorio 11-13/controllers/usuario_controllers.js
@@ -1,19 +1,24 @@
 // Laboratorio 14.
 const path = require('path');
 
-exports.get_signup = (request, response, next) => {
-    response.render('signup.ejs');
-};
-
-exports.post_signup= (request, response, next) => {
-    response.render('signup.ejs');
-    response.redirect('/login');
-};
-
 exports.get_login = (request, response, next) => {
     response.render('login.ejs'); 
 };
 
 exports.login = (request, response, next) => {
     response.redirect('/fe'); 
+};
+
+exports.get_signup = (request, response, next) => {
+    response.render('signup.ejs');
+};
+
+exports.post_signup= (request, response, next) => {
+    const usuario = new User(request.body.nombre, request.body.email, request.body.username, request.body.password);
+    usuario.save()
+    .then(()=> {
+        response.redirect('/usuario/login'); 
+    }).catch((error)=>{
+        console.log(error);
+    });
 };
--- Laboratorio 11-13/routes/usuario.routes.js
+++ Laboratorio 11-13/routes/usuario.routes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 // Referencia a controlador.
 const userController = require('../controllers/usuario_controllers'); 
 
-//router.get('/signup', userController.get_signup);
-//router.post('/signup', userController.post_signup);
+router.get('/signup', userController.get_signup);
+router.post('/signup', userController.post_signup);
 router.get('/login', userController.get_login);
 router.post('/login', userController.login);
 // router.get('/logout', userController.logout);
